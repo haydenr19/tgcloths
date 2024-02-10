58624a78bfcd153b8b1e6221f466db5138232ba8
haydenr19 <159575365+haydenr19@users.noreply.github.com>
2024-02-10 02:57:35 +0000

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,11 @@
+files {
+    'mp_f_freemode_01_mp_f_piercing.meta',
+    'mp_f_freemode_01_mp_f_diadem.meta',
+    'mp_m_freemode_01_mp_m_eupm.meta',
+    'mp_f_freemode_01_mp_f_eupf.meta',
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_piercing.meta' 
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_diadem.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_eupf.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_eupm.meta'
